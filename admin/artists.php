f4b095c6661e095722f2cad17ffed85c42b62578
Tim Linquist <timo3377@gmail.com>
2009-04-21 00:45:49 -0700

    Added artist display and fixed requires for classes in admin/artists.php

--- admin/artists.php
+++ admin/artists.php
@@ -1,11 +1,11 @@
 <?php
-	require( "../class.ArtistFinders.php" );
+	require( "../php/class.ArtistFinders.php" );
 	$artist_finder= new ArtistFinders();
 	$artists= $artist_finder->all_artists();
 
 	if( count($artists) > 0 )
 	{
-		include( "../class.ArtistDisplay.php" );		
+		include( "../php/class.ArtistDisplay.php" );		
 		$artist_display= new ArtistDisplay();		
 		$artist_display->display_artists( $artists );
 	}
@@ -14,5 +14,3 @@
 		echo "<p>No artists are listed at this time.</p>";		
 	}
 ?>
-
-<p>Testing</p>
--- php/class.ArtistDisplay.php
+++ php/class.ArtistDisplay.php
@@ -7,9 +7,16 @@
 
 		public function display_artist( $artist )
 		{
-			echo "<pre>";
-			print_r($artist);
-			echo "</pre>";
+			echo "<div id=\"".$artist->get_id()."\">
+				<p><strong>Name:&nbsp;</strong><span>".$artist->get_name()."</span></p>
+				<p><strong>Biography:</strong></p>
+				<p>".$artist->get_biography()."</p>
+				<div class=\"contact_info\">
+					<p><strong>Contact Information:</strong></p>
+					<p><strong>Phone:&nbsp;</strong><span>".$artist->get_phone()."</span></p>
+					<p><strong>Email:&nbsp;</strong><span>".$artist->get_email()."</span></p>
+				</div>
+			</div>";
 		}
 	}
 ?>
