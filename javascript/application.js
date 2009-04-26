479818c0f691109cee6db6e52aa8d0c37b72a039
Tim Linquist <timo3377@gmail.com>
2009-04-26 10:58:40 -0700

    Added delete confirmation for artists

--- javascript/application.js
+++ javascript/application.js
@@ -3,13 +3,11 @@ jQuery.fn.blindFadeToggle = function(speed, easing, callback) {
 }
 
 function flash_notice_message(msg){
-	console.log("success notice message");
 	flash_message("notice", msg);
 }
 function flash_error_message(msg){
 	flash_message("error", msg);
 }
-
 function flash_message(class_to_add, msg) {
   $("#flash").html("")
 	.removeClass("error notice")
@@ -19,21 +17,25 @@ function flash_message(class_to_add, msg) {
   .blindFadeToggle(2500);	
 }
 
-function delete_artists() {
+function delete_artists() {	
 	$(".delete_artist a").click(function(){
-		chopped_deleted_artist_id= $(this)[0].id.split("_");
-		var deleted_artist_id= chopped_deleted_artist_id[ chopped_deleted_artist_id.length-1 ];	
-		$.ajax({
-		  type: "POST",
-		  url: "delete_artist.php?artist="+deleted_artist_id,
-			success: function(){
-				$("#artist_"+deleted_artist_id).remove();
-				flash_notice_message("Artist successfully deleted");
-			},
-			error: function(){
-				flash_error_message("Unable to delete artist.  Please try again");
-			}
-		});
-		return false;
+		delete_accepted = confirm("Are you sure you want to delete this artist?");
+		if(delete_accepted)
+		{
+			chopped_deleted_artist_id= $(this)[0].id.split("_");
+			var deleted_artist_id= chopped_deleted_artist_id[ chopped_deleted_artist_id.length-1 ];	
+			$.ajax({
+			  type: "POST",
+			  url: "delete_artist.php?artist="+deleted_artist_id,
+				success: function(){
+					$("#artist_"+deleted_artist_id).remove();
+					flash_notice_message("Artist successfully deleted");
+				},
+				error: function(){
+					flash_error_message("Unable to delete artist.  Please try again");
+				}
+			});
+		}
+		return false;			
 	});
 }
