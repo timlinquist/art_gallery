edd7845964f3bea4f8b068a3b13864e11db66d10
Tim Linquist <timo3377@gmail.com>
2009-04-27 13:37:29 -0700

    Added super Form class.  Base class that all forms should inherit from.

--- php/class.EditArtistForm.php
+++ php/class.EditArtistForm.php
@@ -1,54 +1,22 @@
 <?php 
-	class EditArtistForm{
-		
+	require_once("class.Form.php");	
+	class EditArtistForm extends Form {
 		function __construct($artist){ $this->artist= $artist; }
-		function __get($prop_name){ return $this->$prop_name; }
-  	function __set($prop_name, $value ){ $this->$prop_name = $value; }
-
-		public function render()
-		{
-			$action_input= $this->get_action_input_for_controller();
-			echo "<form action='controller.php' method='post' accept-charset='utf-8'>
-							<fieldset>".$this->legend().$this->render_inputs()."</fieldset>
-						</form>";
-		}
 		
 		public function render_inputs()
 		{	
-			return "<div>".$this->get_action_input_for_controller()."</div>".
-						 "<div>".$this->hidden_input("id", $this->artist->get_id())."</div>".
-						 "<div>".$this->input_label("name").$this->text_input("name", $this->artist->get_name())."</div>".
-						 "<div>".$this->input_label("biography")."<br />".$this->textarea_input("biography", $this->artist->get_biography())."</div>".
-						 "<div>".$this->input_label("phone").$this->text_input("phone", $this->artist->get_phone())."</div>".
-						 "<div>".$this->input_label("email").$this->text_input("email", $this->artist->get_email())."</div>".
-						$this->submit_button();
-		}
-		
-		private function legend(){ return "<legend>Edit Artist</legend>"; }
-		private function submit_button(){ return "<input type='submit' value='Submit' />"; }
-		private function input_label($name)
-		{	
-			$display_name= ucwords($name).": ";
-			return "<label for='$name'>$display_name<span class='form_hint'>(required)</span></label>";
-		}
-		private function get_action_input_for_controller()
+			return $this->get_action_input_for_controller().
+						 $this->hidden_input("id", $this->artist->get_id()).
+						 $this->text_input("name", $this->artist->get_name())."<br />".
+						 $this->textarea_input("biography", $this->artist->get_biography()).
+						 $this->text_input("phone", $this->artist->get_phone()).
+						 $this->text_input("email", $this->artist->get_email()).
+						 $this->submit_button();
+		}		
+		protected function legend(){ return "<legend>Edit Artist</legend>"; }		
+		protected function get_action_input_for_controller()
 		{ 
 			return "<input type='hidden' id='action' name='action' value='edit_artist' />"; 
-		}
-		
-		private function hidden_input($field, $value)
-		{
-			return "<input type='hidden' name='$field' id='artist_$field' value='$value' />"; 
-		}
-		
-		private function text_input($field, $value)
-		{
-			return "<input type='text' name='$field' id='artist_$field' value='$value' />"; 			
-		}
-		
-		private function textarea_input($field, $value)
-		{
-			return "<textarea rows='10' cols='60' name='$field' id='artist_$field'>$value</textarea>"; 
 		}
 	}
 ?>
---
+++ php/class.Form.php
@@ -0,0 +1,43 @@
+<?php 
+	class Form{	
+		function __construct(){ }
+		function __get($prop_name){ return $this->$prop_name; }
+  	function __set($prop_name, $value ){ $this->$prop_name = $value; }
+
+		public function render()
+		{
+			$action_input= $this->get_action_input_for_controller();
+			echo "<form action='controller.php' method='post' accept-charset='utf-8'>
+							<fieldset>".$this->legend().$this->render_inputs()."</fieldset>
+						</form>";
+		}
+
+		public function render_inputs(){ return "OVERRIDE ME ( render_inputs() ) IN SUBCLASSES TO PRINT INPUTS FOR SPECIFIC CHILD FORM!"; }
+		
+		protected function legend(){ return "<legend>Edit</legend>"; }		
+		protected function submit_button(){ return "<input type='submit' value='Submit' />"; }
+		protected function hidden_input($field, $value)
+		{
+			return "<input type='hidden' name='$field' id='$field' value='$value' />"; 
+		}
+		protected function text_input($field, $value)
+		{
+			return $this->input_wrapper_start($field)."<input type='text' name='$field' id='$field' value='$value' />".$this->input_wrapper_end(); 			
+		}
+		protected function textarea_input($field, $value)
+		{
+			return $this->input_wrapper_start($field)."<textarea rows='10' cols='60' name='$field' id='$field'>$value</textarea>".$this->input_wrapper_end(); 
+		}
+		
+		private function input_wrapper_start($field)
+		{ 
+			return "<div id='".$field."_wrapper'>".$this->input_label($field); 
+		}
+		private function input_wrapper_end(){ return "</div>"; }
+		private function input_label($field)
+		{	
+			$display_name= ucwords($field).": ";
+			return "<label for='$field'>$display_name<span class='form_hint'>(required)</span></label>";
+		}
+	}
+?>
