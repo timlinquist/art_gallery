40ae9848b7cc425078de576e17949a19de7faf32
adentone <adentone@gmail.com>
2009-04-27 17:36:46 -0700

    added general styles

--- php/header.php
+++ php/header.php
@@ -1,14 +1,115 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-	<head>
-		<script type="text/javascript" src="../javascript/jquery-1.3.2.min.js"></script>
-		<script type="text/javascript" src="../javascript/jquery.form.js"></script>
-		<script type="text/javascript" src="../javascript/jquery.maskedinput-1.2.2.min.js"></script>
-		<script type="text/javascript" src="../javascript/jquery.validate.pack.js"></script>
-		<script type="text/javascript" src="../javascript/jquery.validate.additional_methods.js"></script>
-		<script type="text/javascript" src="../javascript/application.js"></script>
-	</head>
-	<body>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+   <title>The Entree Gallery in Priest Lake, Idaho - Exhibits, Demonstrations, Contemporary Fine Art</title>
+   <meta name="keywords" content="art, artists, show, exhibits, oil, acrylic, watercolor, exhibitions, northwest, idaho, priest lake, contemporary, fine, art, tom wakeley, janene grende, joe kronenberg, amish furniture, art collections, artwork, lodge furnishings" />
+   <meta name="description" content="The Entree Fine Art Gallery offers original and limited edition paintings, sculptures, textiles, handcrafted furniture, and jewelry by many regionally and nationally renowned contemporary artists." />
+   <link rel="stylesheet" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" type="text/css" />
+   <style type="text/css" media="all">
+   #custom-doc { width: 100%; min-width: 250px; } 
+   html {
+     font-family: Arial, Verdana, Helvetica, sans-serif;
+     background: #6d6660;
+   }
+   #bd {
+     font-size: 115%;
+     line-height: 1.5em;
+     text-align: justify;
+     background: #4f473f;
+     color: #ddd;
+     border-top: 2px solid #551413;
+     border-bottom: 2px solid #551413;
+   }
+   #bd h1 { font-weight: bold; font-size: 160%; padding: 0 0 12px 0; }
+   #bd h2 { font-weight: bold; font-size: 140%; padding: 0 0 12px 0; }
+   #bd h3 { font-weight: bold; font-size: 120%; padding: 0 0 12px 0; }
+   #bd p { padding: 0 0 12px 0; }
+   
+   .content {
+     width: 950px;
+     margin: 0 auto;
+   }
+   .yui-g {
+     padding: 1.5em 0 .5em 0;
+   }
+   #logo {
+     display: inline;
+     float: left;
+     padding-bottom: 5px;
+   }
+   #navigation {
+     display: inline;
+     float: right;
+   }
+   #navigation.top {
+     font-size: 110%;
+     position: relative;
+     top: 155px;
+     right: 2px;
+   }
+   #navigation.bottom {
+     position: relative;
+     top: 0;
+     right: 2px;
+   }
+   #navigation ul {
+     list-style-type: none;
+   }
+   #navigation ul li {
+     display: inline;
+     float: left;
+   }
+   #navigation ul li a {
+     padding: 1em 0 1em 2em;
+     color: #ccc;
+     text-decoration: none;
+   }
+   #navigation ul li a:hover {
+     color: #fff;
+   }
+   #ft {
+     color: #ddd;
+     padding: .25em 0;
+   }
+   #copyright {
+     display: inline;
+     float: left;
+   }
+   strong {
+     font-weight: bold;
+     color: #fff;
+   }
+   a img.thumb {
+     border: 2px solid #4f473f;
+   }
+   a:hover img.thumb {
+     border: 2px solid #551413;
+   }
+   </style>
+   <script type="text/javascript" charset="utf-8" src="./javascript/jquery-1.3.2.min.js"></script>
+   <script type="text/javascript" src="../javascript/jquery.form.js"></script>
+	 <script type="text/javascript" src="../javascript/jquery.maskedinput-1.2.2.min.js"></script>
+	 <script type="text/javascript" src="../javascript/jquery.validate.pack.js"></script>
+	 <script type="text/javascript" src="../javascript/jquery.validate.additional_methods.js"></script>
+	 <script type="text/javascript" src="../javascript/application.js"></script>
+</head>
+<body>
+  <div id="custom-doc" class="yui-t7">
+    <div id="hd" class="content">
+      <div id="logo"><img src="./images/logo.png" width="270" height="170" alt="The Entree Gallery Logo" /></div>
+      <div id="navigation" class="top">
+        <ul>
+          <li><a href="#">home</a></li>
+          <li><a href="#">events</a></li>
+          <li><a href="#">feature shows</a></li>
+          <li><a href="#">artists</a></li>
+          <li><a href="#">search</a></li>
+          <li><a href="#">about</a></li>
+          <li><a href="#">contact</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="bd"><div class="yui-g content">
 		<div id="flash">&nbsp;</div>
 
 
---
+++ style_test.php
@@ -0,0 +1,18 @@
+<?php
+  require './php/header.php';
+?>
+
+<div id="artist_details" style="display: inline; float: left; width: 700px;">
+  <h1>Terry Lee</h1>
+  <p>The wild beauty of African wildlife is depicted in the paintings of artist Terry Lee.</p>
+  <p>Capturing his animals in a moment of time and in all of their majestic beauty, his impressionistic style, along with his magnificent sense of color, gives new meaning to Africa's spectacular scenery and wildlife.</p>
+  <p>Terry believes that traveling to Africa and seeing these animals in their native habitat helps him to see with "African Eyes". Terry feels that "I do my best to create a sense of energy and action. I capture emotional moments using color and light, sweeping brush strokes and bold swaths of color, drawing the eye into the scene. I want the viewer to feel the intense light and heat of the sun, creating atmosphere and mood".</p>
+</div>
+<div id="art_examples" style="display: inline; float: right; width: 220px;">
+  <a href="./photos/full_size/rembrandt.jpg" title="this is a caption" class="thickbox" rel="gallery-plants"><img class="thumb" src="./photos/thumb_size/rembrandt.jpg" alt="this is art" /></a>
+  <a href="./photos/full_size/monalisa.jpg"  title="this is a caption" class="thickbox" rel="gallery-plants"><img class="thumb" src="./photos/thumb_size/monalisa.jpg" alt="this is art" /></a>
+</div>
+
+<?php
+  require './php/footer.php';
+?>
