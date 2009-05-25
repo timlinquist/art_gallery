ae155aebda01f128ebcd43823eb4bdd77a653916
Tim Linquist <timo3377@gmail.com>
2009-05-25 13:37:12 -0700

    Forgot to switch to branch. Pagination async and working

--- javascript/art_search.js
+++ javascript/art_search.js
@@ -3,6 +3,7 @@ function unbind_selects_change_event() {
 	$('select').unbind('change');
 }
 function unbind_search_submit() { $("#art_search").unbind('submit'); }
+function unbind_pagination_links() { $(".pagination").unbind('click'); }
 
 function process_response(response){
 	unbind_selects_change_event();
@@ -10,6 +11,24 @@ function process_response(response){
 	$('#'+chopped_response[0]+'_select_wrapper').html(chopped_response[1]);
 	$('#'+chopped_response[2]+'_select_wrapper').html(chopped_response[3]);
 	bind_change_events();
+}
+
+function bind_pagination() {
+	$(".pagination").click(function(){
+		data= $(this).attr('href') ;
+		data= data.split("?");
+		data= data[1];
+		$.ajax({
+		   type: "POST",
+		   url: "./php/do_search.php",
+		   data: data,
+		   success: function(response){
+				$('#search_results').html(response);
+				bind_pagination();
+		   }
+		 });	
+		return false;	
+	});
 }
 
 function reset_form_click() {
@@ -21,10 +40,12 @@ function reset_form_click() {
 		   success: function(response){
 				$('#search_results').html('');
 				unbind_selects_change_event();
+				unbind_pagination_links();
 				unbind_search_submit();
 				reset_form(response);
 				bind_change_events();
 				bind_search_submit();
+				bind_pagination();
 		   }
 		 });		
 	});
@@ -40,6 +61,7 @@ function bind_search_submit() {
 	    beforeSubmit:  function(){ disable_form(); return true;}, 
 	    success: function(response){
 				$('#search_results').html(response);
+				bind_pagination()
 	    	enable_form();
 			},
 			error: function(){ enable_form(); }
--- php/do_search.php
+++ php/do_search.php
@@ -7,7 +7,47 @@
 	  $_POST[$key] = mysql_real_escape_string(trim($value));
 	}
 
-	if(isset($_POST["category_id"]))
+	if(isset($_POST["search"]))
+	{
+		$search_filter= new SearchFilters($_POST["artist"], $_POST["category"], $_POST["medium"]);
+		$art= $search_filter->filter_art_by_all();
+		if(count($art) > 0 && count($art) > 10)
+		{
+			require "Paginated.php";
+			require "DoubleBarLayout.php";
+			
+			$page= ( isset($_GET['page']) ) ? $_GET['page'] : 1;
+			$page= ( $page==1 && isset($_POST['page']) ) ? $_POST['page'] : 1;
+			
+			$paginated_results="";
+			$query_vars= "&search=true&category=" . $_POST["category"] . "&medium=" . $_POST["medium"] . "&artist=" .  $_POST["artist"];
+			$art_display=	new ArtDisplay();			
+			//constructor takes three parameters
+			//1. array to be paged
+			//2. number of results per page (optional parameter. Default is 10)
+			//3. the current page (optional parameter. Default  is 1)
+			$pagedResults = new Paginated($art, 10, $page);
+
+			$paginated_results .= "<ul>";
+
+			while($row = $pagedResults->fetchPagedRow()) {	//when $row is false loop terminates
+				$paginated_results .= "<li>". $art_display->display_art_piece( $row) ."</li>";
+			}
+			$paginated_results .= "</ul>";
+			//important to set the strategy to be used before a call to fetchPagedNavigation
+			$pagedResults->setLayout(new DoubleBarLayout());
+			$paginated_results .= $pagedResults->fetchPagedNavigation( $query_vars );
+			echo $paginated_results;
+		}
+		elseif(count($art) > 0){
+			$art_display=	new ArtDisplay();
+			echo $art_display->display_art( $art );			
+		}
+		else{
+			echo "No art was found for the options you selected.";
+		}
+	}
+	elseif(isset($_POST["category_id"]))
 	{
 		$search_filter= new SearchFilters(null, $_POST["category_id"], null);
 		$search_filter->filtered_by_category();
@@ -26,21 +66,4 @@
 	{
 		$search_form = new ArtSearchForm();
 		echo $search_form->render_form();
-	}
-	elseif(isset($_POST["search"]))
-	{
-		$search_filter= new SearchFilters($_POST["artist"], $_POST["category"], $_POST["medium"]);
-		$art= $search_filter->filter_art_by_all();
-		if(count($art) > 0 && count($art) <= 10)
-		{
-			$art_display=	new ArtDisplay();
-			echo $art_display->display_art( $art );						
-		}
-		elseif(count($art) > 0){
-			$art_display=	new ArtDisplay();
-			echo $art_display->display_art( $art );			
-		}
-		else{
-			echo "No art was found for the options you selected.";
-		}
 	}
