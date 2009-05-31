3ca428fa58e44a33f96e5f8386ede8bf1e918d77
Tim Linquist <timo3377@gmail.com>
2009-05-30 18:59:25 -0700

    Added pagination for public artists and admin artists

--- artists.php
+++ artists.php
@@ -7,9 +7,26 @@
 
 	if( count($artists) > 0 )
 	{
+		require "./php/Paginated.php";
+		require "./php/DoubleBarLayout.php";
 		include "./php/class.ArtistDisplay.php";		
-		$artist_display= new ArtistDisplay();		
-		$artist_display->display_artists( $artists );
+		
+		$artist_display= new ArtistDisplay();
+		
+		$page= ( isset($_GET['page']) ) ? $_GET['page'] : 1;		
+		$pagedResults = new Paginated($artists, 10, $page);
+
+		$paginated_results = "<ul>";
+
+		while($row = $pagedResults->fetchPagedRow()) {	//when $row is false loop terminates
+			$paginated_results .= "<li>". $artist_display->display_artist( $row) ."</li>";
+		}
+		$paginated_results .= "</ul>";
+		//important to set the strategy to be used before a call to fetchPagedNavigation
+		$pagedResults->setLayout(new DoubleBarLayout());
+		$paginated_results .= $pagedResults->fetchPagedNavigation();
+		
+		echo $paginated_results;
 	}
 	else
 	{
