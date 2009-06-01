ec0aa346bae6a6ee05affa372be38c2b01afa0ae
Tim Linquist <timo3377@gmail.com>
2009-05-31 20:40:30 -0700

    Unique filenames for photos

--- php/class.PhotoUploader.php
+++ php/class.PhotoUploader.php
@@ -13,24 +13,25 @@
 		
 		public function upload()
 		{
-			$full_size_upload = self::FULL_SIZE_FOLDER_PATH . basename($this->files['photo_upload']['name']);
+			$uniqified_timestamp= mktime() . "_";
+			$full_size_upload = self::FULL_SIZE_FOLDER_PATH . $uniqified_timestamp . basename($this->files['photo_upload']['name']);
 
 			if(is_uploaded_file($this->files['photo_upload']['tmp_name']))
 			{
-				if (move_uploaded_file($this->files['photo_upload']['tmp_name'], $full_size_upload)) 
+				if(move_uploaded_file($this->files['photo_upload']['tmp_name'], $full_size_upload)) 
 				{
-					$this->generate_resized_photos($full_size_upload);
-					$thumb_url= self::THUMB_SIZE_FOLDER_PATH . basename($this->files['photo_upload']['name']);
-					echo "success|".$thumb_url."|".$this->files['photo_upload']['name'];
+					$this->generate_resized_photos($full_size_upload, $uniqified_timestamp);
+					$thumb_url= self::THUMB_SIZE_FOLDER_PATH . $uniqified_timestamp . basename($this->files['photo_upload']['name']);
+					echo "success|".$thumb_url."|". $uniqified_timestamp . $this->files['photo_upload']['name'];
 					return;
 				} 
 				echo "error";
 			}			
 		}
 		
-		private function generate_resized_photos($full_size_upload)
+		private function generate_resized_photos($full_size_upload, $timestamp)
 		{
-			$thumb_file_name= basename($this->files['photo_upload']['name']);
+			$thumb_file_name= $timestamp . basename($this->files['photo_upload']['name']);
 			$this->resizer= new PhotoResizer($full_size_upload, $thumb_file_name);
 			$this->generate_thumbnail($full_size_upload);
 		}	
