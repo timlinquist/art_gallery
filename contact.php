b60a122f80a1b08042e4a2567053af0dc1b7e86f
adentone <adentone@gmail.com>
2009-05-31 20:17:51 -0600

    added content for the contact page

--- contact.php
+++ contact.php
@@ -1,11 +1,12 @@
 <?php require "./php/header.php"; ?>
 
 <div id="galleries">
-<?php
-	foreach( $GLOBALS['galleries'] as $gallery )	
-	{	 
-		echo $gallery;
-	}
-?>
+  <div style="clear: both;"><p style="display: inline; float: left;"><strong>Reeder Bay Gallery</strong><br />1755 Reeder Bay Rd<br />Nordman&nbsp; ID&nbsp; 83848<br />208-443-2001</p>
+	  <iframe style="margin-left: 2em; display: inline; float: left;" width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;q=Reeder+Bay+Gallery+1755+Reeder+Bay+Rd+Nordman,+Id+83848&amp;fb=1&amp;split=1&amp;gl=us&amp;cid=0,0,635550345020513537&amp;ei=oxsjSuzCMKSwtAOhkuCMBA&amp;ll=48.633223,-116.91781&amp;spn=0.006295,0.006295&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;q=Reeder+Bay+Gallery+1755+Reeder+Bay+Rd+Nordman,+Id+83848&amp;fb=1&amp;split=1&amp;gl=us&amp;cid=0,0,635550345020513537&amp;ei=oxsjSuzCMKSwtAOhkuCMBA&amp;ll=48.632123,-116.91781&amp;spn=0.006295,0.006295&amp;iwloc=A&amp;source=embed" style="display:block;clear:both;color:#ccc;text-align:center">View Larger Map</a></small>
+	</div> 
+	<div style="clear: both; padding: 2em 0;"><p style="display: inline; float: left;"><strong>Coolin Bay Gallery</strong><br />379 Bay View Rd<br />Coolin&nbsp; ID&nbsp; 83821<br />208-443-2002</p>
+	  <iframe style="margin-left: 2em; display: inline; float: left;" width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Coolin+Bay+Gallery+379+Bay+View+Rd+Coolin++ID++83821&amp;sll=48.632738,-116.917791&amp;sspn=0.035169,0.090981&amp;gl=us&amp;ie=UTF8&amp;cid=5352082219759041682&amp;ll=48.489989,-116.84329&amp;spn=0.027304,0.054932&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Coolin+Bay+Gallery+379+Bay+View+Rd+Coolin++ID++83821&amp;sll=48.632738,-116.917791&amp;sspn=0.035169,0.090981&amp;gl=us&amp;ie=UTF8&amp;cid=5352082219759041682&amp;ll=48.489989,-116.84329&amp;spn=0.027304,0.054932&amp;z=14&amp;iwloc=A" style="display:block;clear:both;color:#ccc;text-align:center">View Larger Map</a></small>
+	</div>
 </div>
+
 <?php require "./php/footer.php"; ?>
