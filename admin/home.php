9b6699b26311867cb3d0a4a50f385c3e755d5997
adentone <adentone@gmail.com>
2009-06-02 22:06:14 -0700

    added admin home page

---
+++ admin/home.php
@@ -0,0 +1,9 @@
+<?php 
+  require "admin_header.php";
+?>
+<h1>Website Administration</h1>
+<p>Please use the navigation above or below to modify the site content.</p>
+<p>Please call Aaron (509-216-4388) or Andy (509-991-9394) if you have any questions.</p>
+<?php 
+	require "admin_footer.php"; 
+?>
